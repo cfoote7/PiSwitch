0b4f6a223b075cf7367f521f19718c6d2a177715
cfoote7 <chris.foote@nwmsrocks.com>
2018-02-17 15:46:58 -0800

    Update and rename autostart.sh to setup.sh

--- autostart.sh
+++
@@ -1,5 +0,0 @@
-sudo hciconfig hci0 piscan
-
-date >> /opt/retropie/configs/all/NewTouchBoot/logfile.txt
-
-/opt/retropie/configs/all/NewTouchBoot/starter.sh >> /opt/retropie/configs/all/NewTouchBoot/logfile.txt #auto
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get install python-wxgtk3.0
+git clone  https://github.com/cfoote7/PiSwitch /home/pi/PiSwitch
+cd /home/pi/PiSwitch
+sudo cp /home/pi/PiSwitch/autostart.sh /opt/retropie/configs/all/autostart.sh
+sudo cp -R /home/pi/PiSwitch/NewTouchBoot/ /opt/retropie/configs/all/NewTouchBoot
+sudo cp -R /home/pi/PiSwitch/joymap/ /home/pi/joymap
+sudo cp /home/pi/PiSwitch/config.txt /boot/config.txt
+sudo chmod 777 /home/pi/joymap/* && sudo chmod a+x /home/pi/joymap/*
+sudo chmod 777 /opt/retropie/configs/all/NewTouchBoot/checknum
+sudo chmod 777 /opt/retropie/configs/all/NewTouchBoot/retro.jpg
+sudo chmod 777 /opt/retropie/configs/all/NewTouchBoot/kodi.jpg
+sudo chmod 777 /opt/retropie/configs/all/NewTouchBoot/terminal.png
+sudo chmod 777 /opt/retropie/configs/all/NewTouchBoot/Debian.jpg
+sudo chmod 777 /opt/retropie/configs/all/NewTouchBoot/logfile.txt
+sudo chmod 777 /opt/retropie/configs/all/NewTouchBoot/starter.sh
+sudo chmod a+x /opt/retropie/configs/all/NewTouchBoot/starter.sh
+sudo chmod 777 /opt/retropie/configs/all/NewTouchBoot/pywx.py
+sudo chmod a+x /opt/retropie/configs/all/NewTouchBoot/pywx.py
+sudo chmod 777 /opt/retropie/configs/all/autostart.sh
+sudo chmod a+x /opt/retropie/configs/all/autostart.sh
